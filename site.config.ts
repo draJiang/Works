8172212e4716b07149e7a23f1ca1be02e14693c2
draJiang <598353451@qq.com>
2023-02-05 19:27:34 +0800

    创建项目

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1a1dc74a500a4e30a669c2751c3e46c7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
